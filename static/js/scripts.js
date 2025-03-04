dc7a3fe5928e58b7f178f50f8d2971df36fa1d7f
andyzhang <148032979+andyzhang-THz@users.noreply.github.com>
2025-03-04 22:22:32 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'project','vacancy']
+const section_names = ['home', 'publications', 'project','team','vacancy']
 
 
 window.addEventListener('DOMContentLoaded', event => {
